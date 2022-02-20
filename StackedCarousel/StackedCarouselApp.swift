df81b51cbfe35a9ea65a5c2514b24cf1d9f32859
Stanley Pan <imoment.developer@gmail.com>
2022-02-20 14:13:34 -0500

    Initial Commit

---
+++ StackedCarousel/StackedCarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackedCarouselApp.swift
+//  StackedCarousel
+//
+//  Created by Stanley Pan on 2022/02/20.
+//
+
+import SwiftUI
+
+@main
+struct StackedCarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
