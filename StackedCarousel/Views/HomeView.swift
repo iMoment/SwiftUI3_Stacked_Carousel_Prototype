65ebdbdd36dce29973f8fa428142820c77b32f5e
Stanley Pan <imoment.developer@gmail.com>
2022-02-20 14:36:06 -0500

    Added title UI to HomeView

--- StackedCarousel/Views/HomeView.swift
+++ StackedCarousel/Views/HomeView.swift
@@ -8,9 +8,45 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State var cards: [Card] = [
+        Card(cardColor: Color("customBlue"), date: "Sunday 20th February", title: "Neurobics for your \nmind."),
+        Card(cardColor: Color("customPurple"), date: "Monday 21st February", title: "Brush up on hygiene."),
+        Card(cardColor: Color("customGreen"), date: "Tuesday 22nd February", title: "Don't skip breakfast."),
+        Card(cardColor: Color("customPink"), date: "Wednesday 23rd February", title: "Eat an apple a day."),
+        Card(cardColor: Color("customYellow"), date: "Thursday 24th February", title: "Light walking for 20 minutes."),
+    ]
     
     var body: some View {
-        Text("Hello, World!")
+        
+        VStack {
+            // MARK: Title UI
+            HStack(alignment: .bottom) {
+                
+                VStack(alignment: .leading) {
+                    
+                    Text("20th of FEB")
+                        .font(.largeTitle.bold())
+                    
+                    Label {
+                        Text("Seattle, USA")
+                    } icon: {
+                        Image(systemName: "location.circle")
+                    }
+                }
+                
+                Spacer()
+                
+                Text("Updated 2:32 PM")
+                    .font(.caption2)
+                    .fontWeight(.light)
+            }
+            
+            GeometryReader { proxy in
+                
+            }
+        }
+        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
     }
 }
 
